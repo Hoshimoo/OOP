64f09003caa0282f3eb29c5e1ea3d24fdd1f74d9
Hoshimoo <111482767+Hoshimoo@users.noreply.github.com>
2022-10-04 15:41:31 +0700

    Add files via upload

---
+++ Week7/Main.java
@@ -0,0 +1,23 @@
+//public class Main {
+//   public static void main(String[] args) {
+//       new CalculatorSample();
+// }
+//}
+
+//public class Main {
+//    public static void main(String[] args) { 
+//        new TellerGUI();
+// }
+//}
+
+//public class Main {
+//    public static void main(String[] args) { 
+//        new SimpleCalGUI();
+// }
+//}
+
+public class Main {
+    public static void main(String[] args) {
+        new MnGUI();
+ }
+}
